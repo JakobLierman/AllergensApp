e5fcce0c7f273f048b16c3f8329e544509c38b2f
Jakob Lierman <jakob.lierman@gmail.com>
2018-09-11 18:46:37 +0200

    Added icons to allergens

--- src/domain/Allergen.java
+++ src/domain/Allergen.java
@@ -2,7 +2,11 @@ package domain;
 
 import javafx.beans.property.SimpleStringProperty;
 
+import javax.imageio.ImageIO;
 import javax.persistence.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * The type Allergen.
@@ -17,6 +21,8 @@ public class Allergen {
     @Basic
     @Column(name = "name", nullable = false)
     private String name;
+    @Transient
+    private BufferedImage icon;
 
     public Allergen() {
     }
@@ -30,6 +36,103 @@ public class Allergen {
         if (name.isEmpty())
             throw new NullPointerException("Name can't be empty.");
         this.name = name;
+        setIcon(name);
+    }
+
+    // Sets an icon if one is available
+    private void setIcon(String allergenName) {
+        String url;
+        switch (allergenName) {
+            case ("Almond"):
+                url = "/resources/images/almond.png";
+                break;
+            case ("Amandel"):
+                url = "/resources/images/almond.png";
+                break;
+            case ("Celery"):
+                url = "/resources/images/celery.png";
+                break;
+            case ("Selder"):
+                url = "/resources/images/celery.png";
+                break;
+            case ("Crustaceans"):
+                url = "/resources/images/crustaceans.png";
+                break;
+            case ("Schaaldieren"):
+                url = "/resources/images/crustaceans.png";
+                break;
+            case ("Egg"):
+                url = "/resources/images/egg.png";
+                break;
+            case ("Ei"):
+                url = "/resources/images/egg.png";
+                break;
+            case ("Fish"):
+                url = "/resources/images/fish.png";
+                break;
+            case ("Vis"):
+                url = "/resources/images/fish.png";
+                break;
+            case ("Gluten"):
+                url = "/resources/images/gluten.png";
+                break;
+            case ("Lupin"):
+                url = "/resources/images/lupin.png";
+                break;
+            case ("Lupine"):
+                url = "/resources/images/lupin.png";
+                break;
+            case ("Milk"):
+                url = "/resources/images/milk.png";
+                break;
+            case ("Melk"):
+                url = "/resources/images/milk.png";
+                break;
+            case ("Mollusc"):
+                url = "/resources/images/mollusc.png";
+                break;
+            case ("Weekdier"):
+                url = "/resources/images/mollusc.png";
+                break;
+            case ("Mustard"):
+                url = "/resources/images/mustard.png";
+                break;
+            case ("Mosterd"):
+                url = "/resources/images/mustard.png";
+                break;
+            case ("Peanut"):
+                url = "/resources/images/peanut.png";
+                break;
+            case ("Pinda"):
+                url = "/resources/images/peanut.png";
+                break;
+            case ("Sesame"):
+                url = "/resources/images/sesame.png";
+                break;
+            case ("Sesamzaad"):
+                url = "/resources/images/sesame.png";
+                break;
+            case ("Soybean"):
+                url = "/resources/images/soybean.png";
+                break;
+            case ("Soja"):
+                url = "/resources/images/soybean.png";
+                break;
+            case ("Sulfide"):
+                url = "/resources/images/sulfide.png";
+                break;
+            case ("Sulfiet"):
+                url = "/resources/images/sulfide.png";
+                break;
+            default:
+                url = "/resources/images/notfound.png";
+                break;
+        }
+        try {
+            icon = ImageIO.read(new File(url));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -52,9 +155,14 @@ public class Allergen {
         this.name = name;
     }
 
-    @Override
-    public String toString() {
-        return this.name;
+    /**
+     * Gets icon.
+     *
+     * @return the icon
+     */
+    public BufferedImage getIcon() {
+        return icon;
+    }
 
     /**
      * Name property simple string property.
