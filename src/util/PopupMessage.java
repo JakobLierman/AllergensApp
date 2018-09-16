4dfe122897d112bb23fd7ac7a819902d413f0afc
Jakob Lierman <jakob.lierman@gmail.com>
2018-09-16 17:21:24 +0200

    Finishing up implementation of PopupMessages

--- src/util/PopupMessage.java
+++ src/util/PopupMessage.java
@@ -1,6 +1,8 @@
 package util;
 
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
 import javafx.stage.StageStyle;
 
 /**
@@ -16,15 +18,27 @@ public class PopupMessage {
      * @param title       the title
      * @param headerText  the header text
      * @param contentText the content text
+     * @param yesText     the yes text
+     * @param noText      the no text
+     * @return true if yes is selected
      */
-    public static void showConfirmationMessage(String title, String headerText, String contentText) {
+    public static boolean showConfirmationMessage(String title, String headerText, String contentText, String yesText, String noText) {
         alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.initStyle(StageStyle.UTILITY);
+
         alert.setTitle(title);
         alert.setHeaderText(headerText);
         alert.setContentText(contentText);
-        // TODO - Confirmation buttons
+
+        alert.getButtonTypes().clear();
+        ButtonType yes = new ButtonType(yesText, ButtonBar.ButtonData.YES);
+        ButtonType no = new ButtonType(noText, ButtonBar.ButtonData.NO);
+        alert.getButtonTypes().add(yes);
+        alert.getButtonTypes().add(no);
+
         alert.showAndWait();
+
+        return alert.getResult().equals(yes);
     }
 
     /**
@@ -37,9 +51,11 @@ public class PopupMessage {
     public static void showErrorMessage(String title, String headerText, String contentText) {
         alert = new Alert(Alert.AlertType.ERROR);
         alert.initStyle(StageStyle.UTILITY);
+
         alert.setTitle(title);
         alert.setHeaderText(headerText);
         alert.setContentText(contentText);
+
         alert.showAndWait();
     }
 
@@ -53,9 +69,11 @@ public class PopupMessage {
     public static void showWarningMessage(String title, String headerText, String contentText) {
         alert = new Alert(Alert.AlertType.WARNING);
         alert.initStyle(StageStyle.UTILITY);
+
         alert.setTitle(title);
         alert.setHeaderText(headerText);
         alert.setContentText(contentText);
+
         alert.showAndWait();
     }
 
@@ -69,9 +87,11 @@ public class PopupMessage {
     public static void showInformationMessage(String title, String headerText, String contentText) {
         alert = new Alert(Alert.AlertType.INFORMATION);
         alert.initStyle(StageStyle.UTILITY);
+
         alert.setTitle(title);
         alert.setHeaderText(headerText);
         alert.setContentText(contentText);
+
         alert.showAndWait();
     }
 
