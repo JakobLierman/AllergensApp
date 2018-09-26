c47b931855a525b858ca772639236adead5f697f
Jakob Lierman <jakob.lierman@gmail.com>
2018-09-26 12:21:39 +0200

    Implementation DetailScreen

--- src/gui/DetailScreen.java
+++ src/gui/DetailScreen.java
@@ -2,16 +2,22 @@ package gui;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXListView;
+import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
 import domain.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import util.PopupMessage;
 
 import java.io.IOException;
+import java.util.Set;
 
 /**
  * The type Detail screen.
@@ -26,6 +32,8 @@ public class DetailScreen extends AnchorPane {
     private Text txtTitle;
     @FXML
     private JFXTextField tfName;
+    @FXML
+    private JFXTextArea tfDescription;
     @FXML
     private Text txtTableTitle;
     @FXML
@@ -57,6 +65,15 @@ public class DetailScreen extends AnchorPane {
         productManager = domainController.getProductManager();
         this.item = item;
 
+        // Sets selectionmode and disables/enables description textarea according to type
+        if (item instanceof Product) {
+            lvSelectableItems.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+            tfDescription.setDisable(false);
+        } else {
+            lvSelectableItems.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+            tfDescription.setDisable(true);
+        }
+
         fillList();
         setText();
         fillFields(this.item);
@@ -64,11 +81,12 @@ public class DetailScreen extends AnchorPane {
 
     // Fills list according to type
     private void fillList() {
-        if (item instanceof Product) {
-            // TODO - Implement
-        } else {
-            // TODO - Implement
-        }
+        ObservableList<Object> itemList = FXCollections.observableArrayList();
+        if (item instanceof Product)
+            itemList.addAll(productManager.getIngredients());
+        else
+            itemList.addAll(productManager.getAllergens());
+        lvSelectableItems.setItems(itemList);
     }
 
     // Sets all text items according to type
@@ -76,6 +94,7 @@ public class DetailScreen extends AnchorPane {
         btnSave.setText(domainController.getText("Save"));
         btnCancel.setText(domainController.getText("Cancel"));
         txtSelectMultiple.setText(domainController.getText("selectMultiple"));
+        tfDescription.setPromptText(domainController.getText("Description"));
         if (item instanceof Product) {
             txtTitle.setText(domainController.getText("addProduct"));
             tfName.setPromptText(domainController.getText("nameOf") + " " + domainController.getText("Product").toLowerCase());
@@ -89,7 +108,19 @@ public class DetailScreen extends AnchorPane {
 
     // Fills fields if given item isn't new
     private void fillFields(Object item) {
-        // TODO - Implement
+        if (item instanceof Product) {
+            if (!((Product) item).getName().isEmpty()) {
+                tfName.setText(((Product) item).getName());
+                if (!((Product) item).getDescription().isEmpty())
+                    tfDescription.setText(((Product) item).getDescription());
+                // TODO - Select ingredients
+            }
+        } else {
+            if (!((Ingredient) item).getName().isEmpty()) {
+                tfName.setText(((Ingredient) item).getName());
+                // TODO - Select allergen
+            }
+        }
     }
 
     /**
@@ -110,13 +141,29 @@ public class DetailScreen extends AnchorPane {
      */
     @FXML
     void handleSave(ActionEvent event) {
-        if (item instanceof Product) {
-            // TODO - Implement
-        } else {
-            // TODO - Implement
+        try {
+            if (item instanceof Product) {
+                Set list = Set.of(lvSelectableItems.getSelectionModel().getSelectedItems().toArray());
+                if (((Product) item).getName().isEmpty())
+                    productManager.createProduct(tfName.getText(), tfDescription.getText(), list);
+                else
+                    productManager.alterProduct(((Product) item).getName(), tfName.getText(), tfDescription.getText(), list);
+            } else {
+                if (((Ingredient) item).getName().isEmpty())
+                    productManager.createIngredient(tfName.getText(), (Allergen) lvSelectableItems.getSelectionModel().getSelectedItem());
+                else
+                    productManager.alterIngredient(((Ingredient) item).getName(), tfName.getText(), (Allergen) lvSelectableItems.getSelectionModel().getSelectedItem());
+            }
+        } catch (Exception e) {
+            PopupMessage.showErrorMessage(
+                    domainController.getText("Oops"),
+                    domainController.getText("Wrong"),
+                    e.getMessage());
         }
 
-        // Catch exceptions!
+        // Close window
+        stage = (Stage) getScene().getWindow();
+        stage.close();
     }
 
 }
